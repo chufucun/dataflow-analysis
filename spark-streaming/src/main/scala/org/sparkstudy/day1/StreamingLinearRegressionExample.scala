47a2d15bb38d79af29d20f85d52d845e2b20a7da
tianhonghan <affect1210@163.com>
2018-05-29 16:57:33 +0800

    测试数据目录

--- spark-streaming/src/main/scala/org/sparkstudy/day1/StreamingLinearRegressionExample.scala
+++ spark-streaming/src/main/scala/org/sparkstudy/day1/StreamingLinearRegressionExample.scala
@@ -7,13 +7,19 @@ import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.regression.StreamingLinearRegressionWithSGD
 // $example off$
 import org.apache.spark.streaming._
+
+/**
+  * 注:textFileStream 在windows下不能监控文件创建、移动等操作
+  *    通过py_tools子模块的iris_newdata来自动生成 训练和测试 的数据集。
+  *
+  */
 object StreamingLinearRegressionExample {
 
   /**
-    *  在一个数据流上训练回归模型，然后在另一个数据流上进行预测，这两个数据流中，数据都以文本的形式存入两个不同的目录。
-    *  文本文件的行必须用如下格式标记根据点：'(y,[x1,x2,x3,...,xn])'，y是标签,n 表示特征的数量。
-    *  用于训练和测试的n必须是一致的（拥有相同维度的特征）。 
-    *  使用方法:StreamingLinearRegressionExample <训练目录> <测试目录>
+    * 在一个数据流上训练回归模型，然后在另一个数据流上进行预测，这两个数据流中，数据都以文本的形式存入两个不同的目录。
+    * 文本文件的行必须用如下格式标记根据点：'(y,[x1,x2,x3,...,xn])'，y是标签,n 表示特征的数量。
+    * 用于训练和测试的n必须是一致的（拥有相同维度的特征）。 
+    * 使用方法:StreamingLinearRegressionExample <训练目录> <测试目录>
     *  在本机运行时，使用两个目录：训练目录和测试目录，每五分钟更新一次，每个数据点两个特征，调用:
     *  $ bin/run-example mllib.StreamingLinearRegressionExample trainingDir testDir
     *  随着不断的向trainingDir添加文本文件，模式会持续更新。
@@ -22,23 +28,31 @@ object StreamingLinearRegressionExample {
     */
   def main(args: Array[String]): Unit = {
 
-    // 临时用
-
+    var trainingDir = "/mllib/training"
+    var testDir = "/mllib/test"
 
     if (args.length != 2) {
       System.err.println("Usage: StreamingLinearRegressionExample <trainingDir> <testDir>")
-      System.exit(1)
+      //      System.exit(1)
+      println("Read Local Parameter!")
+      trainingDir = getClass().getResource(trainingDir).getPath
+      testDir = getClass().getResource(testDir).getPath
+    } else {
+      trainingDir = args(0)
+      testDir = args(1)
     }
 
     val conf = new SparkConf().setAppName("StreamingLinearRegressionExample")
     conf.setMaster("local[2]")
-    val ssc = new StreamingContext(conf, Seconds(1))
+    val ssc = new StreamingContext(conf, Seconds(5))
 
     // $example on$
-    val trainingData = ssc.textFileStream(args(0)).map(LabeledPoint.parse).cache()
-    val testData = ssc.textFileStream(args(1)).map(LabeledPoint.parse)
+    val trainingLines = ssc.textFileStream(trainingDir)
+    val trainingData = trainingLines.map(LabeledPoint.parse)
 
-    val numFeatures = 3
+    val testData = ssc.textFileStream(testDir).map(LabeledPoint.parse)
+
+    val numFeatures = 4
     val model = new StreamingLinearRegressionWithSGD()
       .setInitialWeights(Vectors.zeros(numFeatures))
 
